3b699c49e1100de94ac742af652fb126bf3c0ac3
vivekbeema <vivekbeema88@gmail.com>
2021-11-22 14:38:41 +0530

    hai this is done

---
+++ src/org/hm/Repository.java
@@ -0,0 +1,11 @@
+package org.hm;
+
+public class Repository {
+private void type() {
+System.out.println("haiiii");
+}
+public static void main(String[] args) {
+	Repository re = new Repository();
+	re.type();
+}
+}
